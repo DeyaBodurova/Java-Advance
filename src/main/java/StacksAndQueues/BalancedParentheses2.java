e21db32e6ec31312e9a13e5045dcbc532e3df8f2
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-09 07:01:43 +0300

    wip

---
+++ src/main/java/StacksAndQueues/BalancedParentheses2.java
@@ -0,0 +1,12 @@
+package Advance.StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BalancedParentheses2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        //ArrayDeque<Character>
+    }
+}
