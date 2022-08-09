7c7101cc420bc4c40d0be0d83b54fd99d02139c8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-09 07:00:00 +0300

    wip

---
+++ src/main/java/MultidimensionalArrays/MultidimensionalArraysMatrixOfPalindromes.java
@@ -0,0 +1,16 @@
+package Advance.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MultidimensionalArraysMatrixOfPalindromes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //TODO to check it in judge before commit to github
+
+        String[] data = sc.nextLine().split("\\s+");
+        int r = Integer.parseInt(data[0]);
+        int c = Integer.parseInt(data[1]);
+        char[][] matrix = new char[r][c];
+    }
+}
