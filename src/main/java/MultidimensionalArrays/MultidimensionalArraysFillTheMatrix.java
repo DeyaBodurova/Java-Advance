cd2094af2811a06213d42753b0cee91de8bcb107
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-09 06:58:14 +0300

    wip

---
+++ src/main/java/MultidimensionalArrays/MultidimensionalArraysFillTheMatrix.java
@@ -0,0 +1,60 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MultidimensionalArraysFillTheMatrix {
+    public static void main(String[] args) {
+
+        //TODO to check it in judge before commit to github
+
+        Scanner sc = new Scanner(System.in);
+        String[] data = sc.nextLine().split(", ");
+        int size = Integer.parseInt(data[0]);
+        String type = data[1];
+        int[][] matrix = new int[size][size];
+
+        if (type.toUpperCase().equals("A")) {
+            patternA(matrix);
+        } else if (type.toUpperCase().equals("B")) {
+            patternB(matrix);
+        }
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void patternA(int[][] matrix) {
+        int n = 1;
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row <= matrix.length - 1; row++) {
+                matrix[row][col] = n;
+                n++;
+            }
+        }
+    }
+
+    private static void patternB(int[][] matrix) {
+        int m = 1;
+        for (int col = 0; col < matrix.length; col++) {
+            if (col % 2 == 0) {
+                for (int row = 0; row < matrix.length; row++) {
+                    matrix[row][col] = m;
+                    m++;
+                }
+            } else if (col % 2 != 0) {
+                for (int row = matrix.length - 1; row >= 0; row--) {
+                    matrix[row][col] = m;
+                    m++;
+                }
+            }
+
+        }
+    }
+}
